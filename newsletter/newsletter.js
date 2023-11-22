72a9d06e617887c7aa9f1baf8beaee00f04d3f65
Pablo <pablo_24_29@hotmail.com>
2023-11-22 11:52:06 +0100

    añadida carpeta icons y newsletter

---
+++ newsletter/newsletter.js
@@ -0,0 +1,27 @@
+// Select the button and input
+var button = document.querySelector('.button-newsletter');
+var input = document.querySelector('.newsletter-input');
+
+// Add a listen event to the button
+button.addEventListener('click', function() {
+    // Collect the value of the input
+    var email = input.value;
+
+    // Create a regular expression to validate emails
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    // Check if the email is valid
+    if (emailRegex.test(email)) {
+        Swal.fire(
+            '¡Suscripción exitosa!',
+            'Gracias por suscribirte a nuestra newsletter, ' + email + '!',
+            'success'
+        );
+    } else {
+        Swal.fire(
+            'Error',
+            'Por favor, introduce un email válido.',
+            'error'
+        );
+    }
+});
