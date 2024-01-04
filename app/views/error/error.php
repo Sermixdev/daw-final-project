6c719d6f2e3dbe172cf18fa0b6b25663c7fabf96
Pablo <pablo_24_29@hotmail.com>
2024-01-04 18:55:39 +0100

    Added error folder within Views with error files. Inserted in the styles css classes of the php error file

---
+++ app/views/error/error.php
@@ -0,0 +1,70 @@
+<style>
+    body {
+        background-image: url("error404.png");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        font-family: 'Irish Grover', Arial, sans-serif;
+        color: #000000;
+        text-align: center;
+        padding: 50px;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    h1 {
+        font-size: 3em;
+        margin-bottom: 10px;
+        color: #27627b;
+    }
+
+    h2 {
+        font-size: 2em;
+        margin-bottom: 20px;
+        color: #1d4f63;
+    }
+
+    p {
+        font-size: 1.2em;
+        margin-bottom: 30px;
+        color: #1d4f63;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 1em;
+        background-color: #27627b;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        border-radius: 50px;
+    }
+
+    button:hover {
+        background-color: #1d4f63;
+    }
+</style>
+
+<body>
+    <div class="bodyError">
+        <div style="
+        background-color: rgba(255, 255, 255, 0.8);
+        padding: 20px;
+        border-radius: 15px;
+      ">
+            <h1 class="errorH1">¡Oops!</h1>
+            <h2 class="errorH2">Parece que hemos extraviado algunas piezas por el camino</h2>
+            <p class="errorP">
+                Mientras las buscamos, siéntete libre de explorar otras secciones,
+                ¡quizás descubras un nuevo juego para tu colección!
+            </p>
+            <button class="errorButton" onclick="window.location.href = 'index.php';">
+                Volver al Inicio
+            </button>
+        </div>
+    </div>
+</body>
