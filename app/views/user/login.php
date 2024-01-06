756b19f856a441741a371b6028ab37eeef2c03c9
Sermixdev <sermixdev@gmail.com>
2024-01-06 20:40:09 +0100

    Updates form styles, adds global css variables, adds required html fields

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -1,15 +1,13 @@
 <!--we start the views with <body> but the </body> is in the index-->
 <body>
-<form method="post" action="<?=base_url?>User/checkLogin" method="post">
-    <fieldset id="loginBox">
-        <legend>
-            <h2>Introduzca el nombre de usuario y contraseña:</h2>
-        </legend>
+<form class="form" method="post" action="<?=base_url?>User/checkLogin" method="post">
+    <h2>Introduzca el nombre de usuario y contraseña:</h2>
+    <fieldset class="form-fieldset">
         <div>
             <label for="userName">Usuario:</label>
-            <input type="text" id="userName" name="userName" /><br /><br />
+            <input type="text" id="userName" name="userName" required/>
             <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" /><br /><br />
+            <input type="password" id="password" name="password" required/>
             <input
                     class="button"
                     type="submit"
@@ -17,9 +15,9 @@
                     value="Login"
             />
         </div>
+        <p>¿No tienes una cuenta?</p> 
+        <a class="button" href="<?=base_url?>user/register">Regístrate</a>
     </fieldset>
-    <br />
 </form>
 
-<p>¿No tienes una cuenta? <a href="<?=base_url?>user/register">REGÍSTRATE AQUÍ</a></p>
 
--- app/views/user/register.php
+++ app/views/user/register.php
@@ -1,30 +1,25 @@
 <!--we start the views with <body> but the </body> is in the index-->
 <body>
-<form method="post" action="<?=base_url?>User/newUser" method="post">
+<form class="form" method="post" action="<?=base_url?>User/newUser" method="post">
     <h2>¡Bienvenidx a la familia Gecomme!</h2>
-    <fieldset id="loginBox">
+    <h3>Introduce tus datos para crear una nueva cuenta</h3>
+    <fieldset class="form-fieldset">
 
-        <legend>
-            <h3>Introduce tus datos para crear una nueva cuenta:</h3>
-        </legend>
-        <div>
             <label for="nameSurname">Nombre y Apellidos:</label>
-            <input type="text" id="nameSurname" name="nameSurname" /><br /><br />
+            <input type="text" id="nameSurname" name="nameSurname" required/>
             <label for="userName">Usuario:</label>
-            <input type="text" id="userName" name="userName" /><br /><br />
+            <input type="text" id="userName" name="userName" required/>
             <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" /><br /><br />
+            <input type="password" id="password" name="password" required/>
             <label for="email">Email:</label>
-            <input type="text" id="email" name="email" /><br /><br />
+            <input type="text" id="email" name="email" required/>
             <label for="address">Dirección de envío completa:</label>
-            <input type="text" id="address" name="address" /><br /><br />
+            <input type="text" id="address" name="address" required/>
             <input
                     class="button"
                     type="submit"
                     name="registerSubmit"
                     value="Registrarse"
             />
-        </div>
     </fieldset>
-    <br />
 </form>
