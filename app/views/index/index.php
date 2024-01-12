bd8b05c6a3e9a01ffbfe2d6978e82ee18f9ff135
Sermixdev <sermixdev@gmail.com>
2024-01-13 00:16:24 +0100

    Changes styles and classes from index.php and styles.css

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,56 +1,43 @@
-<div id="lastFive" class="divProductsInIndex productsList">
-    <h2 id="lastFiveTitle" class="h2ProductsInIndex h2ProductsListTitle">Novedades</h2>
+<div id="productSectionLastFive" class="productContainer">
+    <h2 class="sectionTitle">Novedades</h2>
+    <div class="productList">
     <?php while ($row = mysqli_fetch_array($result)) {
-        extract($row);?>
-        <div class="productOfFive productOf">
-            <div class="divImageOfFive divImage">
+        extract($row); ?>
+        <div class="card">
+            <div class="card-image">
                 <a href="<?=base_url?>Product/productDetail&id=<?php echo $ID_Producto?>">
-                    <img src="<?=base_url?>public/images/pdp/<?php echo $RutaImagen?>" class="imageOfFive imageOf">
+                    <img src="<?=base_url?>public/images/pdp/<?php echo $RutaImagen?>" alt="Product Image">
                 </a>
             </div>
-            <div class="detailOfFive detailOf">
-                <div class="titleOfFive titleOf"><?php echo $NombreProducto?></div>
-                <div class="brandOfFive brandOf"><?php echo $Editorial?></div>
-                <div class="stockOfFive stockOf"><?php if ($Stock==0){
-                    echo "Stock: no";
-                    }else{
-                    echo "Stock: sí";
-                    } ?>
-                </div>
-                <div class="prizeOfFive prizeOf"><?php echo $Precio?></div>
+            <div class="card-details">
+                <div class="card-title"><?php echo $NombreProducto?></div>
+                <div class="card-brand"><?php echo $Editorial?></div>
+                <div class="card-stock"><?php echo $Stock==0 ? "Stock: no" : "Stock: sí"; ?></div>
+                <div class="card-price"><?php echo $Precio?></div>
             </div>
         </div>
-    <?php
-    }
-    ?>
+        <?php } ?>
+    </div>
 </div>
 
-
-<div id="childFive" class="divProductsInIndex productsList">
-    <h2 id="childFiveTitle" class="h2ProductsInIndex h2ProductsListTitle">Para los más peques</h2>
+<div id="productSectionChildFive" class="productContainer">
+    <h2 class="sectionTitle">Para los más peques</h2>
+    <div class="productList">
     <?php while ($row = mysqli_fetch_array($childResult)) {
-        extract($row);?>
-        <div class="productOfFive productOf">
-            <div class="divImageOfFive divImageOf">
+        extract($row); ?>
+        <div class="card">
+            <div class="card-image">
                 <a href="<?=base_url?>Product/productDetail&id=<?php echo $ID_Producto?>">
-                    <img src="<?=base_url?>public/images/pdp/<?php echo $RutaImagen?>" class="imageOfFive imageOf">
+                    <img src="<?=base_url?>public/images/pdp/<?php echo $RutaImagen?>" alt="Product Image">
                 </a>
             </div>
-            <div class="detailOfFive detailOf">
-                <div class="titleOfFive titleOf"><?php echo $NombreProducto?></div>
-                <div class="brandOfFive brandOf"><?php echo $Editorial?></div>
-                <div class="ageOfFive ageOf">Edad mínima:<?php echo $EdadMinima?></div>
-                <div class="stockOfFive stockOf"><?php if ($Stock==0){
-                    echo "Stock: no";
-                    }else{
-                    echo "Stock: sí";
-                    } ?>
-                </div>
-                <div class="prizeOfFive prizeOf"><?php echo $Precio?></div>
+            <div class="card-details">
+                <div class="card-title"><?php echo $NombreProducto?></div>
+                <div class="card-brand"><?php echo $Editorial?></div>
+                <div class="card-stock"><?php echo $Stock==0 ? "Stock: no" : "Stock: sí"; ?></div>
+                <div class="card-price"><?php echo $Precio?></div>
             </div>
         </div>
-    <?php
-    }
-    ?>
+        <?php } ?>
+    </div>
 </div>
-<?php
